9dee5c99ce90ab9b857dd8f4045fa9ce67af606b
Ian Kimani <ianmwng@gmail.com>
2024-01-15 20:21:54 +0300

    complete assingment

--- server/app.py
+++ server/app.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+#!/usr/bin/env python3
+
 from flask import Flask, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
@@ -17,7 +19,22 @@ db.init_app(app)
 api = Api(app)
 
 class Home(Resource):
-    pass
+
+    def get(self):
+
+        response_dict = {
+            "message": "Welcome to the Newsletter RESTful API",
+        }
+
+        response = make_response(
+            response_dict,
+            200
+        )
+
+        return response
+
+api.add_resource(Home, '/')
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
